3ade189580751759c7b0fe75120c4255b1aae60d
Hoàng Lịch Nguyễn <108135808+lichnguyengithub162003@users.noreply.github.com>
2023-05-21 09:39:30 +0700

    first commit

---
+++ 12.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logo</title>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="shotcut icon" href="./img/face15.gif">
+
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" /> -->
+</head>
+
+<body>
+    <?php include "./layout/header.php"; ?>
+
+
+    <!----------------- Banner -------------------------->
+    <section class="banner"></section>
+
+
+
+    <!-- ------------- SẢN PHẨM NỔI BẬT -------------- -->
+    <section class="slide-product">
+        <div class="slide-box">
+
+            <!-- Img -->
+            <div class="slide-img">
+                <img src="./img/285182-hover.webp" alt="1">
+
+                <div class="overlay">
+                    <a href="" class="buy-btn"><i class="fa-solid fa-cart-plus"></i></a>
+                </div>
+            </div>
+
+            <!-- Chi tiết sản phẩm -->
+            <div class="detail-box">
+
+                <!-- Loại sản phẩm -->
+                <div class="type">
+                    <a href="">Ghế sofa</a>
+                    <span>Ghế</span>
+                </div>
+
+                <!-- Giá -->
+                <a href="#" class="price">$56</a>
+            </div>
+        </div>
+        <div class="slide-box">
+
+            <!-- Img -->
+            <div class="slide-img">
+                <img src="./img/285182-hover.webp" alt="1">
+
+                <div class="overlay">
+                    <a href="" class="buy-btn"><i class="fa-solid fa-cart-plus"></i></a>
+                </div>
+            </div>
+
+            <!-- Chi tiết sản phẩm -->
+            <div class="detail-box">
+
+                <!-- Loại sản phẩm -->
+                <div class="type">
+                    <a href="">Ghế sofa</a>
+                    <span>Ghế</span>
+                </div>
+
+                <!-- Giá -->
+                <a href="#" class="price">$56</a>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- ------------- Giới thiệu chung ------------- -->
+    <section class="content1">
+
+        <!-- Left -->
+        <article class="content1-left">
+            <section>
+                <h4>LOGO STORE</h4>
+            </section>
+            <section>
+                <h1>Canada's Source for Premium Window Treatments</h1>
+            </section>
+        </article>
+
+
+        <!-- Right -->
+        <article class="content1-right">
+            <section>
+                <p>Shadings is Canada's premier dealer of window shades, blinds, curtains, as well as outdoor shading solutions. Every sale is backed by a team of skilled artisans, talented consultants and experienced installers who work together to provide exemplary customer service.</p><br>
+                <p>In addition to our selection of premium window blinds, shades and curtains from the industry's leading manufacturers, our custom window treatment services include everything from design consultation to window measurements to installation.</p>
+            </section>
+
+            <section><a href="">See more</a></section>
+        </article>
+
+    </section>
+
+
+
+    <!-- ------------ Image indoor ------------- -->
+    <section class="indoor">
+        <section class="indoor-img">
+            <img src="./img/Trắng và Be.png" alt="">
+        </section>
+    </section>
+
+
+    <!-- ----------- Indoor content ------------ -->
+    <section class="content2">
+
+        <!-- Left -->
+        <article class="content2-left">
+            <section class="content2-right-img">
+                <img src="./img/lion.jpg" alt="">
+                <img src="./img/dining room1.jpg" alt="">
+            </section>
+        </article>
+
+
+        <!-- Right -->
+        <article class="content2-right">
+
+            <section>
+                <h1>Indoor Window Coverings</h1>
+            </section>
+
+            <section>
+                <p>Our vast product selection lets you achieve the style you want in any room of your home. Choose from a bountiful palette of colours, textures, styles, and motorization options to find the perfect balance of form and function with your window blinds and shades.</p> <br>
+                <p>With help from the experts at Shadings, every detail of your window coverings can be perfectly integrated with your interior design vision.</p>
+            </section>
+        </article>
+
+    </section>
+
+
+
+    <!-- ------------ Option indoor ------------- -->
+    <section class="option-indoor">
+        <section class="option">
+
+            <section class="option-img">
+                <img src="./img/shape.jpg" alt="">
+            </section>
+
+            <section class="option-img">
+                <img src="./img/blind.webp" alt="">
+            </section>
+
+            <section class="option-img">
+                <img src="./img/curtain.webp" alt="">
+            </section>
+
+        </section>
+    </section>
+
+
+
+
+    <!-- ======================================== -->
+
+    <!-- ------------ Image outdoor ------------- -->
+    <section class="outdoor">
+        <section class="outdoor-img">
+            <img src="./img/out2.jpg" alt="">
+        </section>
+    </section>
+
+
+
+    <!-- ----------- Outdoor content ------------ -->
+    <section class="content3">
+
+        <!-- Left -->
+        <article class="content3-left">
+            <section>
+                <h1>Exterior Shading Solutions</h1>
+            </section>
+
+            <section>
+                <p>With our selection of premium outdoor shades, screens, awnings and pergolas, you can turn your backyard into an urban oasis.</p> <br>
+                <p>Offering privacy and protection from the elements, our exterior shading solutions are more than just durable and practical – they elevate your outdoor living spaces. Our shades and awnings can be tailored to suit the style of any home, thanks to multiple colour, style and motorization options.</p> <br>
+                <p>If you want to make your balcony or terrace a natural extension of your living space, Shadings also offers STOBAG awning systems and pavilions. Displaying the finest Swiss engineering and design, they offer multiple expansion and automation options.</p>
+            </section>
+        </article>
+
+
+        <!-- Right -->
+        <article class="content3-right">
+
+            <section class="content3-right-img">
+                <img src="./img/pergola1.jpg" alt="">
+                <img src="./img/out11.jpg" alt="">
+            </section>
+
+        </article>
+
+    </section>
+
+
+
+    <!-- ------------ Option outdoor ------------- -->
+    <section class="option-outdoor">
+        <section class="option">
+
+            <section class="option-img">
+                <img src="./img/out7.webp" alt="">
+            </section>
+
+            <section class="option-img">
+                <img src="./img/out8.webp" alt="">
+            </section>
+
+            <section class="option-img">
+                <img src="./img/pergola2.jpg" alt="">
+            </section>
+
+        </section>
+    </section>
+
+
+
+
+
+
+
+
+    <!-- --------------------- Architecture quotes ---------------------->
+
+    <!-- <section class="home" id="home">
+        <section class="swiper home-slider">
+
+            <section class="quotes">
+
+                
+                <section class="swiper-slide slide">
+                    <section class="quote-item">
+                        <section class="quote"><i class="fa-solid fa-quote-left"></i></section>
+                        <section class="quotes-content">
+                            <p> “When I’m working on a problem, I never think about its beauty, I just think about how to solve the problem, but when I finish it, if the solution is not beautiful, I know it’s wrong.”</p>
+                        </section>
+                        <section class="author">- Richard Buckminster Fuller</section>
+                    </section>
+                </section>
+
+            </section>
+
+            <section class="swiper-pagination"></section> 
+
+        </section>
+    </section> -->
+
+
+
+
+
+
+
+
+    <!-- ------------------------------ -->
+
+    <?php include "./layout/footer.php"; ?>
+</body>
+
+<script type="text/javascript" src="./js/index.js"></script>
+<!-- <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script> -->
+</html>
